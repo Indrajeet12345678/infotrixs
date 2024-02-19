076e563e1dde115e0bc33799ecadc7a137bc68c8
Indrajeet <indrajeet0525@gmail.com>
2024-02-19 21:55:23 +0530

    Bug fix

--- StringMatching.cpp
+++ StringMatching.cpp
@@ -10,7 +10,7 @@ void relationalOperation(string s1, string s2)
 	{
 		cout << s1 << " is not equal to " << s2 << endl;
 		if (s1 > s2)
-			cout << s1 << " is greater than " << s2 << endl;
+			cout << s1 << " is not lesser than " << s2 << endl;
 		else
 			cout << s2 << " is greater than " << s1 << endl;
 	}
